1adbcad06934ec3fd0e2e6ec7f463399df14776f
Bhavishm009 <bhavishm009@gmail.com>
2023-03-02 15:50:58 +0530

    Homepage updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,24 @@ import RegisterPage from "LoginPageMFE/RegisterPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "LoginPageMFE/LoginPage";
 import Aboutus from "./components/views/Aboutus/Aboutus";
-// import  from "LoginPageMFE/RoutingComponant";
+import Centerlizer from "LoginPageMFE/Centerlizer";
 import SingleProductPage from 'ProductPageMFE/SingleProductCard';
 import Navbar from "./components/views/Navbar/Navbar";
 const App = () => (
   <>
     <BrowserRouter>
+   
      <Navbar/>
       <Routes>
+     
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signIn" element={<RegisterPage />} />
         <Route path="/aboutus" element={<Aboutus />} />
         <Route path='/product' element={<SingleProductPage/>}/>
+        
       </Routes>
-
+      
     </BrowserRouter>
     {/* <RoutingComponant path='/' element='<HomePage/>'/> */}
     {/* <RoutingComponant routes={routes} path={'/'} element={<HomePage />} /> */}
